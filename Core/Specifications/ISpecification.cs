9c28fb33acde87b1c506e6008a3ac5ea69bd1669
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-05-18 17:41:58 +0400

    Encapsulated parameters in ProductSpecParams class for my GetProducts method in Products controller.Built pagination logic with skip and take and Applied paging in Specification class as well.

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
@@ -12,5 +13,8 @@ namespace Core.Specifications
         List<Expression<Func<T, object>>> Includes {get;}
         Expression<Func<T, object>> OrderBy {get;}
         Expression<Func<T, object>> OrderByDescending {get;}
+        int Take {get;}
+        int Skip {get;}
+        bool IsPagingEnabled{get;}
     }
 }
