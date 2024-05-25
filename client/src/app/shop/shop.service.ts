d367caeced8994ef36912c961ce86734ce959f1c
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-05-25 20:36:44 +0400

    Added shop.service where I created method to get products.Injected that service in appropriate component.Built a simple UI for displaying purposes of products in a shape of card.Improving UI next..

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseApiUrl = 'https://localhost:5001/api/'
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(){
+    return this.http.get<Pagination<Product[]>>(this.baseApiUrl + 'products?pageSize=50');
+  }
 }
