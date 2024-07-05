5b389cd893e2aa0633c1a0c1d31d381e8f782d43
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-07-05 19:10:17 +0400

    Added interceptor for catching error purposes. Implemented ngx toastr notifications on errors for client sied.

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent {
   baseUrl = environment.apiUrl;
+  validationErrors: string[] = [];
 
   constructor (private http: HttpClient) { }
 
@@ -32,8 +33,11 @@ export class TestErrorComponent {
   }
   get400ValidationError(){
     this.http.get(this.baseUrl + 'products/fortytwo').subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error)
+      next: response =>  console.log(response),
+      error: error => {
+        console.log(error),
+        this.validationErrors = error.errors;
+      }
     })
   }
 
