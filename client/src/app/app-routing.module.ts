6faecab4a92f5bc7a53b5974c5bb94baa898491c
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-07-05 18:14:23 +0400

    Implemented lazy loading of shop-component on client side. In addition created errors template where I can test all the status code errors from API. The latter provides versatility and flexibility during developement process.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'shop', component: ShopComponent},
-  {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
 
