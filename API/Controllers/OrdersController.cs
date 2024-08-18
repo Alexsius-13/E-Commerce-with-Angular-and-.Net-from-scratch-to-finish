adae57c96010174b42df7c7dcfd47b184ddfbe14
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-08-18 23:09:53 +0400

    Extend mapping configuration for getting relevant Dto Results.

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -33,17 +33,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Order>>> GetOrdersForUser()
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
             var orders = await _orderService.GetOrdersForUserAsync(email);
 
-            return Ok(orders);
+            return Ok(_mapper.Map<IReadOnlyList<OrderToReturnDto>>(orders));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Order>> GetOrderByIdForUser(int id) 
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id) 
         {
             var email = HttpContext.User.RetrieveEmailFromPrincipal();
 
@@ -51,7 +51,7 @@ namespace API.Controllers
 
             if(order == null) return NotFound(new ApiResponse(404));
 
-            return Ok(order);
+            return _mapper.Map<OrderToReturnDto>(order);
         }
 
         [HttpGet("deliveryMethods")]
