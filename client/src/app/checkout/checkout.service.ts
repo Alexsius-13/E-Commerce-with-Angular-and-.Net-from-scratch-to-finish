6162133534daabe242e3c4dcdded72351cf260e9
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-08-26 19:48:09 +0400

    Fully implemented order checkout basic structural functionality.Integrating stripe payment in future.

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { DeliveryMethod } from '../shared/models/deliveryMethod';
 import { map } from 'rxjs';
+import { Order, OrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class CheckoutService {
 
 
   constructor(private http: HttpClient) { }
+
+  createOrder(order: OrderToCreate) {
+    return this.http.post<Order>(this.baseApiUrl + 'orders', order);
+  }
 
   getDeliveryMerhods() {
     return this.http.get<DeliveryMethod[]>(this.baseApiUrl + 'orders/deliveryMethods').pipe(
